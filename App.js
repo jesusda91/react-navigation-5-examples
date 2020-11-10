77b341e7da7b32a39d764fcac5a2cecd3e31cc26
Jesus Martinez <jesus.martinez@vecindario.com>
2020-11-09 20:17:51 -0500

    add two screen with navigation

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen({ navigation, extraData }) {
+	const { id } = extraData;
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+			<Text>{id}</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Details Screen</Text>
+		</View>
+	);
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home">
+				<Stack.Screen name="Home" options={{ title: 'Overview' }}>
+					{props => <HomeScreen {...props} extraData={{ id: 1 }} />}
+				</Stack.Screen>
+				<Stack.Screen name="Details" component={DetailsScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default App;
